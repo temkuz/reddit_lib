100913c88eb5b554334e2e528aa158a8f7535978
temkuz <temkuz@yandex.ru>
2022-12-10 21:38:13 +0500

    Change packages in setup

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='reddit_lib',
     version='0.1.0',
-    packages=['reddit_lib', 'reddit_lib.classes', 'reddit_lib.parsers'],
+    packages=find_packages(),
     author='temkuz',
     author_email='temkuz@bk.ru',
     description='Python library for parsing reddit without using api',
